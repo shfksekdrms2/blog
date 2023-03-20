1ef7b9ed7e5011b02d710eb75b013847c2388730
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2023-03-20 22:13:41 +0900

    - 예외 처리 추가

--- src/main/java/com/solution/blog/domain/search/controller/BlogSearchController.java
+++ src/main/java/com/solution/blog/domain/search/controller/BlogSearchController.java
@@ -6,11 +6,17 @@ import com.solution.blog.domain.search.controller.model.SortType;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Api(tags = {"블로그 검색"})
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class BlogSearchController {
@@ -23,10 +29,13 @@ public class BlogSearchController {
                     "pagination, \n\n" +
                     "다음 -> 네이버 검색순")
     @GetMapping("/blog/search")
-    public BlogSearchRs search(@RequestParam(value = "keyword") String keyword,
+    public BlogSearchRs search(@RequestParam(value = "keyword")
+                               @NotBlank String keyword,
                                @RequestParam(value = "sortType", required = false, defaultValue = "ACCURACY") SortType sortType,
-                               @RequestParam(value = "page", required = false, defaultValue = "1") Integer page,
-                               @RequestParam(value = "size", required = false, defaultValue = "10") Integer size) {
+                               @RequestParam(value = "page", required = false, defaultValue = "1")
+                               @Min(1) @Max(50) Integer page,
+                               @RequestParam(value = "size", required = false, defaultValue = "10")
+                               @Min(1) @Max(50) Integer size) {
         return blogComponent.searchBlog(keyword, sortType, page, size);
     }
 
